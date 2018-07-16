01e7fcc0e73c6c8e91cc5a5090f783b8e5290137
Дмитрий Матыцин <zlodm@yandex.ru>
2018-07-16 16:19:47 +0300

    Add corrected DZ2

--- DZ2/1.php
+++ DZ2/1.php
@@ -1,6 +1,11 @@
-<html>
+<!doctype html>
+<html lang="ru">
 <head>
-    <title>Домашка 2</title>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Таблица истинности</title>
 </head>
 <body>
 <h3>Конъюнкция</h3>
