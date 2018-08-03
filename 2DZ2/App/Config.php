e3fa7361a654c1c351a75e81e448053ab076e74e
Дмитрий Матыцин <zlodm@yandex.ru>
2018-08-03 17:19:52 +0300

    Add 2DZ2

---
+++ 2DZ2/App/Config.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+class Config
+{
+    public $data;
+
+    protected static $config;
+
+    protected function __construct() //защищённый конструктор
+    {
+        $this->data = require __DIR__ . '/../config.php';
+    }
+
+    public static function instance()
+    {
+        if ( null === self::$config ) {
+            self::$config = new self;
+        }
+
+        return self::$config;
+    }
+}
+
